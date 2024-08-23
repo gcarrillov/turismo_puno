d00e3d6c69763029a008805b2a5b91a03ef5c162
gcarrillov <139822114+gcarrillov@users.noreply.github.com>
2024-08-23 07:00:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Definir los sitios turísticos con sus respectivos montos mínimos
 const sitiosTuristicos = [
-    { nombre: "Lago Titicaca", montoMinimo: 50, url: "sitios_turisticos.html#lago-titicaca" },
-    { nombre: "Isla Taquile", montoMinimo: 70, url: "sitios_turisticos.html#isla-taquile" },
-    { nombre: "Sillustani", montoMinimo: 40, url: "sitios_turisticos.html#sillustani" },
-    { nombre: "Catedral de Puno", montoMinimo: 20, url: "sitios_turisticos.html#catedral-puno" },
-    { nombre: "Chullpas de Cutimbo", montoMinimo: 30, url: "sitios_turisticos.html#chullpas-cutimbo" }
+    { nombre: "La Isla de Amantani", montoMinimo: 60, url: "sitios_turisticos.html#amantani" },
+    { nombre: "El Cementerio Sillustani", montoMinimo: 50, url: "sitios_turisticos.html#Sillustani" },
+    { nombre: "El Cañon de Tijani", montoMinimo: 40, url: "sitios_turisticos.html#tijani" },
+    { nombre: "Lampa Pukara", montoMinimo: 30, url: "sitios_turisticos.html#lampa" },
+    { nombre: "Aramu Muro", montoMinimo: 30, url: "sitios_turisticos.html#aramu_muro" },
+    { nombre: "Chullpas de Molloco", montoMinimo: 30, url: "sitios_turisticos.html#molloco" },
+    { nombre: "La ciudad de Chucuito", montoMinimo: 30, url: "sitios_turisticos.html#site1" },
+    { nombre: "Las Islas FLotantes de los Uros", montoMinimo: 30, url: "sitios_turisticos.html#uros" },
+    { nombre: "El Museo Dreyer", montoMinimo: 20, url: "sitios_turisticos.html#dreyer" },
+    { nombre: "El Cerrito Huajsapata", montoMinimo: 20, url: "sitios_turisticos.html#huajsapata"},
+    { nombre: "Arco Deustua", montoMinimo: 10, url: "sitios_turisticos.html#site2" },
+    { nombre: "Parque Pino", montoMinimo: 10, url: "sitios_turisticos.html#pino" },
+    { nombre: "La Catedral", montoMinimo: 10, url: "sitios_turisticos.html#catedral" },
+    { nombre: "El Mirador de Kuntur Wasi", montoMinimo: 10, url: "sitios_turisticos.html#kuntur" },
+    { nombre: "El Mirador de Puma Uta", montoMinimo: 10, url: "sitios_turisticos.html#puma" },
 ];
 
 // Función para filtrar los sitios turísticos según el monto ingresado
